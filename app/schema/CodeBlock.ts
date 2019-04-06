6d4136ca4d0552d45bbd364e97dd62fa680eb569
Gordon Goodrum <stopgordy@gmail.com>
2019-04-06 13:36:10 -0500

    codeblock

--- app/schema/CodeBlock.ts
+++ app/schema/CodeBlock.ts
@@ -23,9 +23,7 @@ export class CodeBlock extends FieldSchema implements BlockInterface {
 		return new Promise(function (resolve, reject) {
 
 			let output = [];
-
 			output.push(`<div class="what settings-apply-container"><p>` + self.value + `</p></div>`);
-
 			resolve(output.join(''));
 		});
 
